43af137c3abaa6ff275a51fa10b21c813902a37c
Ummu Rasul <ummurasul5@gmail.com>
2019-08-27 16:42:15 +0300

    did migration

--- videonews/admin.py
+++ videonews/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Post, Video, News, ContactUs
+from .models import Post, Video, News, ContactUs
 
 # Register your models here.
 admin.site.register(Post)
