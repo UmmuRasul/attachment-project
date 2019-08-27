acfaa7547a7949e4a10e227c89e38634123ba43d
Ummu Rasul <ummurasul5@gmail.com>
2019-08-27 15:26:44 +0300

    added database

--- vbn/settings.py
+++ vbn/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storages',
+    'videonews',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,15 @@ WSGI_APPLICATION = 'vbn.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'default':{
+	    'ENGINE':'django.db.backends.postgresql',
+	    'NAME': 'attachment',
+	    'USER': 'postgres',
+	    'PASSWORD': 'ummu123',
+	    'HOST': 'localhost'
 }
+}
+
 
 
 # Password validation
